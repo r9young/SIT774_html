49f9f59d806a131c111ee3e6b6b1f601a5754191
Your Name <r9young@hotmail.com>
2024-09-09 12:26:23 +1000

    update

--- 8.1P_Creating_a_Node_Project/index.js
+++ 8.1P_Creating_a_Node_Project/index.js
@@ -1,5 +1,6 @@
 // Require the express web application framework (https://expressjs.com)
 const express = require('express')
+const path = require('path'); 
 
 // Additional package for logging of HTTP requests/responses
 const morgan = require('morgan');
@@ -21,4 +22,42 @@ app.listen(port, ()=> {
   // how to shut the server down.
   console.log(`Web server running at: http://localhost:${port}`)
   console.log(`Type Ctrl+C to shut down the web server`)
+})
+
+
+app.use(express.static(path.join(__dirname, 'public')));
+// when a client visits http://localhost:3000/, Express will automatically serve the index.html file from the public directory (if it exists).
+
+app.get('/foreerror', (req, res) => {
+  try {
+    console.log('Got a request to force an error ...');
+    let f;
+    console.log(`f=${f.nomethod()}`);  // This will throw a TypeError
+  } catch (error) {
+    console.error('An error occurred:', error.message);  // Log the error
+    res.status(500).send("500: TypeError: Cannot read properties of undefined (reading 'nomethod')");  // Send a 500 response to the client
+  }
+});
+
+
+app.get('/foreerror', (req, res) => {
+  try {
+    console.log('Got a request to force an error ...');
+    let f; // empty variable
+    // will cause an error as f does not have a method called nomethod
+    // leaving f undefined and then attempting to call a method on it.
+    console.log(`f=${f.nomethod()}`);  // This will throw a TypeError
+  } catch (error) {
+    console.error('An error occurred:', error.message);  // Log the error
+    res.status(500).send("500: TypeError: Cannot read properties of undefined (reading 'nomethod')");  // Send a 500 response to the client
+  }
+});
+
+//create error handler
+// localhost:3000/doenotexist
+// 404: File not found
+
+app.get('/doenotexist', (req, res) => {
+  console.log('File not found');
+  res.status(404).send('File not found');
 })
--- 8.1P_Creating_a_Node_Project/note.md
+++ 8.1P_Creating_a_Node_Project/note.md
@@ -1,3 +1,11 @@
 `nodemon`
 
 The purpose of using nodemon is to automatically restart your Node.js application when it detects changes in the source files.
+
+
+http://localhost:3000/
+
+const path = require('path'); 
+
+app.use(express.static(path.join(__dirname, 'public')));
+// when a client visits http://localhost:3000/, Express will automatically serve the index.html file from the public directory (if it exists).
