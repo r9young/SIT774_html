56fd34d4750ff6aab20b36b897fc8db312f06f6f
Your Name <r9young@hotmail.com>
2024-09-13 11:43:12 +1000

    update 8.2C bug: yellow

--- 8.2C_server_app/8.2C_main/express.js
+++ 8.2C_server_app/8.2C_main/express.js
@@ -5,35 +5,58 @@ const morgan = require('morgan');
 const app = express ();
 const port = 3000;
 
-// Have the logging code
-app.use(morgan('common'));
-
-
-app.use(express.static('public_html')); 
-
-// Serve static files like index.html and index.js
-
-
-
 // Tell our application to serve all the files under the 'public_html directory app use(express.static('public_html'))
 // < ADD GLOABL VARIABLES HERE >>
 // ....
+
+app.use(express.static('public_html')); 
+// the purpose of app.use(express.static('public_html'));  is to tell Expresss: Look in the public_html folder whenever a file request comes in
+
+// otherwise: 404: Error: ENOENT: no such file or directory, stat '/Users/r9young/Library/Mobile Documents/com~apple~CloudDocs/Coding/html - SIT774/8.2C_server_app/8.2C_mainpublic_html/index.html'
+
+
+
+
 
 app. get('/', (req, res) => {
     res.sendFile(__dirname + "public_html/index.html")
     // __dirname is a global variable in Node.js that represents the directory name of the current module (the file that is being executed).
 })
 
-// Route to handle the 'like' action
+
+
+// < ADD CODE HERE >>
+// The handler for a GET request on route '/likegrey' and return a dynamic page (fragment)
+// ...
+// Route to handle the 'likegrey' action
+
 app.post('/likegrey', (req, res) => {
     // Log request body (optional)
     console.log(req.body);
-  
+
     // Send an HTML page with the message
+    res.send(`
+        <html>
+        <body>
+            <h1>You liked 'Grey'!</h1>
+            <p>Thank you for your vote. We appreciate your feedback.</p>
+            <a href="/">Go back to the main page</a>
+        </body>
+        </html>
+    `);
+});
+
+
+// < ADD CODE HERE >>
+// The handler for a GET request on route '/likeyellow' and return a dynamic page (fragment)
+// ...
+// Route to handle the 'likeyellow' action
+app.post('/likeyellow', (req, res) => {
+    console.log(req.body);
     res.send(`
       <html>
         <body>
-          <h1>You liked 'Grey'!</h1>
+          <h1>You liked 'Yellow'!</h1>
           <p>Thank you for your vote. We appreciate your feedback.</p>
           <a href="/">Go back to the main page</a>
         </body>
@@ -42,13 +65,9 @@ app.post('/likegrey', (req, res) => {
   });
   
   
-// < ADD CODE HERE >>
-// The handler for a GET request on route '/likeyellow' and return a dynamic page (fragment)
-// ...
+  
 
-// < ADD CODE HERE >>
-// The handler for a GET request on route '/likegrey' and return a dynamic page (fragment)
-// ...
+
 
 // The last route handler can be used to return your own error messages
 // it is expecting an 'Error' object as the first parameter, which is generated
--- 8.2C_server_app/8.2C_main/public_html/index.js
+++ 8.2C_server_app/8.2C_main/public_html/index.js
@@ -1,5 +1,5 @@
 
-
+//Grey Cap
 document.getElementById('likeGreyBtn').addEventListener('click', () => {
 
   console.log("User click the button");
@@ -7,6 +7,12 @@ document.getElementById('likeGreyBtn').addEventListener('click', () => {
   fetch('/likegrey', {
     method: 'POST'  // Correct way to set the HTTP method
   })
+
+  // by using "fetch & POST", the client-side JavaScript is telling the server-side JavaScript to handle the request.
+  // The Express.js server listens for incoming requests.
+  // the POST request made by the client is directly related to the response (res) sent by the server.  
+  //The POST request and response (res) are two sides of the same interaction
+
   .then(response => response.text())
   .then(data => {
     console.log(data);
@@ -16,14 +22,23 @@ document.getElementById('likeGreyBtn').addEventListener('click', () => {
 });
 
 
-// document.getElementById('likeGreyBtn').addEventListener('click', () => {
-//   fetch('/likegrey', {
-//     method: 'POST',
-//   })
-//   .then(response => response.text())  // Convert response to text
-//   .then(data => {
-//     // Replace the current page content with the server response (the new HTML page)
-//     document.documentElement.innerHTML = data;
-//   })
-//   .catch(error => console.error('Error:', error));
-// });
+document.getElementById('likeYellowBtn').addEventListener('click', () => {
+
+  console.log("User click the button");
+  
+  fetch('http://localhost:3000/likeyellow', {
+    // console.log(data);
+    method: 'POST'  // Correct way to set the HTTP method
+  })
+  // by using "fetch & POST", the client-side JavaScript is telling the server-side JavaScript to handle the request.
+  // The Express.js server listens for incoming requests.
+  // the POST request made by the client is directly related to the response (res) sent by the server.  
+  //The POST request and response (res) are two sides of the same interaction
+
+  .then(response => response.text())
+  .then(data => {
+    // console.log(data);
+    // alert(data)})
+  })
+  .catch(error => console.error('Error:', error));
+});
