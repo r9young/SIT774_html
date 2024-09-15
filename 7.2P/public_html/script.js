62467b323f0b462f118e98fc8161b581f6e988a7
Your Name <r9young@hotmail.com>
2024-09-15 19:01:15 +1000

    update 7.2P

--- 7.2P/public_html/script.js
+++ 7.2P/public_html/script.js
@@ -9,10 +9,40 @@ const iso = document.getElementById('iso');
 
 btn.addEventListener('click', outputMessage);
 
-function outputMessage() {
+
+let count = 1;
+
+function outputMessage(e) {
+    e.preventDefault();
     let data = new Date();
     output.style.display = 'block';
-    attime.textContent = `At time: ${data}`;
-    ondate.textContent = `On date: ${data.toLocaleString()}`;
-    iso.textContent = `The date in ISO formate: ${data.toISOString()}`;
+    attime.textContent = `At time: ${data.toTimeString()}`;
+    ondate.textContent = `On date: ${data.toDateString()}`;
+    iso.textContent = `The date in ISO format: ${data.toISOString()}`;
+
+    output.onmouseover = function() {
+        output.classList.add('output-visible');
+    };
+
+    output.onmouseout = function() {
+        output.classList.remove('output-visible'); 
+    };
+
+
+
+  
+    if (count < 4 ) {
+        btn.textContent = `Try again... Press ${count} times`;
+    } else if (count < 5) {
+        btn.textContent = `Try again... Press ${count} times`;
+        output.style.display = 'none';
+    } else {
+        console.log (`${count} times`)
+        count = 0;
+        btn.textContent = "Click to Restart"
+        output.style.display = 'none';
+    }
+
+    count ++;
+
 }
